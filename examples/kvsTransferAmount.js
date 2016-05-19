1422960259cc595a84d4706c7e6f3243e82af4ee
Kliment Mamykin <kmamyk@gmail.com>
2016-05-19 14:48:21 -0400

    Added tape tests

--- examples/kvsTransferAmount.js
+++ examples/kvsTransferAmount.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const {effects: storage} = require('./effects/storage')
+const { effects: storage } = require('./effects/storage')
 
 function * transferAmount (from, to, amount) {
   assert(from && from.length > 0, 'from is empty')
@@ -11,11 +11,11 @@ function * transferAmount (from, to, amount) {
   const toBalance = yield storage.getItem(`accounts:${to}`)
   assert(typeof toBalance !== 'undefined', 'to account not found')
   yield storage.setItem(`accounts:${from}`, fromBalance - amount)
-  yield storage.setItem(`accounts:${to}`, toBalance  + amount)
+  yield storage.setItem(`accounts:${to}`, toBalance + amount)
   return amount
 }
 
-function * setupAccount(number, initial) {
+function * setupAccount (number, initial) {
   return yield storage.setItem(`accounts:${number}`, initial)
 }
 function * transferThrough (from, through, to, amount) {
@@ -25,19 +25,30 @@ function * transferThrough (from, through, to, amount) {
   return amount
 }
 
-const {createRunner} = require('../src/index')
-const {mapRunner} = require('./effects/storage')
+const { createRunner } = require('../src/index')
+const { mapRunner } = require('./effects/storage')
 
 const map = new Map([['accounts:yours', 10000000], ['accounts:mine', 0]])
 const run = createRunner(mapRunner(map))
 
-run(transferAmount('yours', 'mine', 1000)).then(console.log, console.error).then(() => console.log(map))
-run(transferThrough('yours', 'intermediary', 'mine', 1000)).then(console.log, console.error).then(() => console.log(map))
+// run examples sequentially
+Promise.resolve().then(() =>
+  run(transferAmount('yours', 'mine', 1000)).then(console.log, console.error).then(() => console.log(map))
+).then(() =>
+  run(transferThrough('yours', 'intermediary', 'mine', 1000)).then(console.log, console.error).then(() => console.log(map))
+).then(() =>
+  run(storage.getItem(`accounts:yours`)).then(console.log, console.error).then(() => console.log(map))
+)
 
-//playByPlay(transferAmount('yours', 'mine', 1000))
-//  .expect(storage.getItem(`accounts:yours`), 1000)
-//  .expect(storage.getItem(`accounts:mine`), 0)
-//  .expect(storage.setItem('accounts:yours', 0))
-//  .expect(storage.setItem('accounts:mine', 1000))
-//  .returns(1000)
-//  .then(console.log, console.error)
+const test = require('tape')
+test('transferAmount', function (t) {
+  t.end()
+  //playByPlay(transferAmount('yours', 'mine', 1000))
+  //  .expect(storage.getItem(`accounts:yours`), 1000)
+  //  .expect(storage.getItem(`accounts:mine`), 0)
+  //  .expect(storage.setItem('accounts:yours', 0))
+  //  .expect(storage.setItem('accounts:mine', 1000))
+  //  .returns(1000)
+  //  .then(console.log, console.error)
+})
+
