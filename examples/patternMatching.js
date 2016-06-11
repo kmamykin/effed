f1b1df00bb67500f5f76a5041fa316453ddd5785
Kliment Mamykin <kmamyk@gmail.com>
2016-06-11 10:59:03 -0400

    Working on tests

--- examples/patternMatching.js
+++ examples/patternMatching.js
@@ -1,29 +1,16 @@
 const union = require('../src/union')
+
 // define
-const effects = union({
-  saveGame: ['x', 'y'],
-  restoreGame: []
-})
-
-// create
-const e = effects.saveGame({x: 2, y: 4})
-
-// match
-console.log(e)
-e.match({
-  saveGame: ({x, y}) => {},
-  restoreGame: () => {},
-  _: () => {}
-})
-
 const List = union({
   Nil: [],
   Cons: ['x', 'xs']
 })
 
+// create
 const l1 = List.Cons(2, List.Cons(3, List.Cons(4, List.Nil())))
 
-const length = (list) => list.match({ 
+// match
+const length = (list) => list.match({
   Nil: 0,
   Cons: ({x, xs}) => (1 + length(xs))
 })
@@ -35,11 +22,14 @@ const product = (list) => list.match({
   Nil: 1,
   Cons: ({x, xs}) => (x * product(xs))
 })
-
+const head = (list) => list.match({
+  Cons: ({x, xs}) => x,
+  _: () => null
+})
 console.log(l1)
 console.log(l1.toString())
 console.log(Object.prototype.toString.call(l1)) // need to use this in toString impl to work?
 console.log(length(l1))
 console.log(sum(l1))
 console.log(product(l1))
-
+console.log(head(l1))
--- src/union.js
+++ src/union.js
@@ -1,6 +1,6 @@
 // TODO: move union in a different npm module
 const util = require('util')
-const identity = x => x
+const returnVoid = x => {}
 const TAG = Symbol('TAG')
 const TYPE = Symbol('TYPE')
 
@@ -19,11 +19,12 @@ const union = (types, mixins = {}) => {
     // important to use function () ... not =>, so 'this' is not lexical, but the actual object instance
     const proto = {
       // toString: () => proto.inspect(),
-      inspect: function () {
-        return `${typeName}(${util.inspect(Object.assign({}, this))})`
-      },
+      // inspect: function () {
+      //   return `${typeName}(${util.inspect(Object.assign({}, this))})` // using Object.assign({}, this) so util.inspect does not infinitely recurse
+      // },
+      type: typeName,
       match: function (cases) {
-        const { [typeName]: matchedCase, _: defaultCase = identity } = cases
+        const { [typeName]: matchedCase, _: defaultCase = returnVoid } = cases
         return typeof matchedCase === 'function'
           ? matchedCase(this)
           : (typeof matchedCase === 'undefined' ? defaultCase(this) : matchedCase)
@@ -33,7 +34,6 @@ const union = (types, mixins = {}) => {
       const params = parseConstructorParams(typeName, ...args)
       // These properties are hidden but will be used to in the prototype (deep)compare instances of types
       const properties = {
-        [TYPE]: typeName
       }
       return Object.assign(Object.create(proto), properties, params, mixins)
     }
